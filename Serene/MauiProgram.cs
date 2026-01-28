050807d54e96554139c6b0f2cbf89eafb411ad5e
ArpeetNeupane <neupanearpeet@gmail.com>
2026-01-28 11:36:43 +0545

    enhance: fixing export to pdf by adding to adroidmanifest.xml

--- Serene/MauiProgram.cs
+++ Serene/MauiProgram.cs
@@ -2,6 +2,8 @@
 using Serene.Data;
 using Microsoft.Extensions.Logging;
 using Blazor.Sonner.Extensions;
+using QuestPDF.Infrastructure;
+using CommunityToolkit.Maui;
 
 namespace Serene
 {
@@ -9,9 +11,14 @@ namespace Serene
     {
         public static MauiApp CreateMauiApp()
         {
+            //setting questpdf license
+            QuestPDF.Settings.License = LicenseType.Community;
+
             var builder = MauiApp.CreateBuilder();
+
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit() //important for file saving
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("WorkSans-Regular.ttf", "WorkSansRegular");
@@ -20,19 +27,24 @@ namespace Serene
             builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-            //registering AppDbContext for DI
+            //registering appdbcontext for dependency injection
             builder.Services.AddDbContext<AppDbContext>();
             builder.Services.AddBlazorWebViewDeveloperTools();
             builder.Services.AddSonner();
+
             builder.Services.AddSingleton<ThemeService>();
             builder.Services.AddSingleton<ILoggerService, LoggerService>();
             builder.Services.AddSingleton<IAuthService, AuthService>();
-            builder.Services.AddScoped<IJournalService,  JournalService>();
+            builder.Services.AddScoped<IJournalService, JournalService>();
             builder.Services.AddScoped<IStreakService, StreakService>();
             builder.Services.AddScoped<IAnalyticsService, AnalyticsService>();
+            builder.Services.AddScoped<IExportService, ExportService>();
+
             builder.Logging.AddDebug();
 #endif
+
             var app = builder.Build();
+
             using (var scope = app.Services.CreateScope())
             {
                 var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
